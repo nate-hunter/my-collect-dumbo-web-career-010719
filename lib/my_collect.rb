2bc778055fb291e0c453ee5db7a80c51311b74b6
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-27 04:01:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect (collection)
+	mod_collection = []
+	i = 0
+
+	while i < collection.length
+      mod_collection << yield(collection[i])
+      i += 1
+		end
+		
+		mod_collection
+
+end
 
 
