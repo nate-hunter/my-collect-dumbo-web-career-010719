a0a409cd886690f3425a1784035d6220cf2f4d7b
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-27 04:00:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect (collection)
+	mod_collection = []
+	i = 0
+
+	while i < collection.length
+      mod_collection << yield(collection[i])
+      
+      (collection[i]) do |element|
+      mod_collection << element
+      i += 1
+		end
+		
+		mod_collection
+
+end
 
 
